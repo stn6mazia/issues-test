ee2f51d5941fdb54f4c95839ac6d409ae5cb9751
Nicolas Mazia <nicolas.mazia@zrp.com.br>
2018-12-18 19:20:59 -0200

    done

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,13 +7,18 @@ class TodoList extends Component {
     super(props);
 
     this.state = {
-      itens: []
+      itens: [],
+      search: true,
+      create: false,
+      searchInput: ''
     }
 
     this.addItem = this.addItem.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
+    this.editItem = this.editItem.bind(this);
   }
 
+  
   addItem(e) {
     if (this._inputElement.value !== '') {
       let newItem = {
@@ -21,17 +26,39 @@ class TodoList extends Component {
         key: Date.now(),
         status: true
       };
-
+      
       this.setState((prevState) => {
         return {
           itens: prevState.itens.concat(newItem)
         }
       });
-
+      
       this._inputElement.value = '';
-
+      
       e.preventDefault();
     }
+  }
+  
+  editItem(newValue) {
+    let itemKey = this.state.itens.find(item => item.key)
+    itemKey.text = newValue;
+    this.setState({
+      itemKey
+    });
+  }
+
+  searchItem() {
+    this.setState({
+      search: true,
+      create: false
+    })
+  }
+
+  createItem() {
+    this.setState({
+      search: false,
+      create: true
+    })
   }
 
   deleteItem(key) {
@@ -44,7 +71,18 @@ class TodoList extends Component {
     })
   }
 
+  updateSearch(evt) {
+    this.setState({
+      searchInput: evt.target.value.substr(0, 20)
+    });
+  }
+
   render() {
+    let filteredItems = this.state.itens.filter(
+      (item) => {
+        return item.text.indexOf(this.state.searchInput) !== -1;
+      }
+    )
     return (
       <div className="todoListMain">
         <div className="container">
@@ -52,10 +90,33 @@ class TodoList extends Component {
             <div className="col-3"></div>
             <div className="col-6">
               <h1>OLAR</h1>
-              <form onSubmit={this.addItem} className="row">
-                <input ref={(a) => this._inputElement = a} type="text" className="form-control col-10" placeholder="create a Item"></input>
-                <button type="submit" className="btn btn-primary col-2"> Add Item </button>
-              </form>
+              <div className="row">
+                <div className="col-6 buton-top">
+                  <button className="btn btn-success" onClick={() => this.createItem()}>Create New Item</button>
+                </div>
+                <div className="col-6 buton-top">
+                  <button className="btn btn-default" onClick={() => this.searchItem()}>Search Item</button>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12">
+                  {
+                    this.state.create ?
+                      <form onSubmit={this.addItem} className="row">
+                        <input ref={(a) => this._inputElement = a} type="text" className="form-control col-10" placeholder="Create Item"></input>
+                        <button type="submit" className="btn btn-success col-2"> Add Item </button>
+                      </form>
+                      : null
+                  }
+                  {
+                    this.state.search ?
+                      <div className="row">
+                        <input type="text" className="form-control col-12" placeholder="Seach Item" value={this.state.searchInput} onChange={this.updateSearch.bind(this)}></input>
+                      </div>
+                      : null
+                  }
+                </div>
+              </div>
             </div>
             <div className="col-3"></div>
           </div>
@@ -63,7 +124,12 @@ class TodoList extends Component {
             <div className="col-2"></div>
             <div className="col-8">
               <div className="issues-box">
-                <Issues entries={this.state.itens} delete={this.deleteItem}/>
+                {
+                  !this.state.itens ?
+                    <p className="no-issue">You have no Issues</p>
+                    : null
+                }
+                <Issues entries={this.state.itens} delete={this.deleteItem} editItem={this.editItem} />
               </div>
             </div>
             <div className="col-2"></div>
--- src/components/issues.js
+++ src/components/issues.js
@@ -1,39 +1,99 @@
 import React, { Component } from 'react';
-import FlipMove from 'react-flip-move';
 import './components.scss';
 
 class Issues extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      editing: false,
+      done: false
+    }
+
     this.createIssue = this.createIssue.bind(this);
+    this.update = this.update.bind(this);
   }
 
   createIssue(item) {
     return (
-    <li class="issue-item" key={item.key}>
-      <div className="row">
-        <div className="col-10 issue-text">
-          <p>{item.text}</p>
+      <li className="issue-item" key={item.key}>
+        <div className="row">
+          <div className="col-9 issue-text">
+            {
+              !this.state.editing ?
+                <p>{item.text}</p>
+                : null
+            }
+            {
+              this.state.editing ?
+                <form onSubmit={this.update}>
+                  <div className="row">
+                    <div className="col-10">
+                      <input className="form-control editing" type="text" defaultValue={item.text} ref={(value) => this.input = value} />
+                    </div>
+                    <div className="col-2">
+                      <button className="btn btn-success" type="submit" onClick={this.update} >Save</button>
+                    </div>
+                  </div>
+                </form>
+                : null
+            }
+          </div>
+          <div className="col-1 issue-status">
+            {
+              !this.state.editing ?
+                <a onClick={() => this.edit()} className="no-decoration">
+                  <span class="fas fa-pencil-alt edit"></span>
+                </a>
+                : null
+            }
+          </div>
+          <div className="col-1 issue-status">
+            {
+              !this.state.done ?
+                <a onClick={() => {
+                  item.status = false;
+                  this.changeStatus()
+                  }} className="no-decoration">
+                  <span class="fas fa-check check"></span>
+                </a>
+                : null
+            }
+          </div>
+          <div className="col-1 issue-status">
+            <a onClick={() => this.delete(item.key)} className="no-decoration">
+              <span class="fas fa-times x"></span>
+            </a>
+          </div>
         </div>
-        <div className="col-1 issue-status">
-          <a onClick={() => item.status = false} className="no-decoration">
-            <span class="fas fa-check check"></span>
-          </a>
-        </div>
-        <div className="col-1 issue-status">
-          <a onClick={() => this.delete(item.key)} className="no-decoration">
-            <span class="fas fa-times x"></span>
-          </a>
-        </div>
-      </div>
-    </li>
+      </li>
     )
   }
 
   delete(key) {
     this.props.delete(key);
   }
+
+  edit() {
+    this.setState({
+      editing: true
+    });
+  }
+
+  changeStatus() {
+    this.setState({
+      done: true
+    });
+  }
+
+  update(e) {
+    e.preventDefault();
+    this.setState({
+      editing: false
+    })
+    this.props.editItem(this.input.value)
+  }
+
   render() {
     let todoEntries = this.props.entries;
     let itens = todoEntries.map(this.createIssue);
